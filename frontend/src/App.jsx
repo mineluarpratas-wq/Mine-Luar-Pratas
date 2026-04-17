cd6ea7140c439a140586791b7b743abb49638ee0
Apple <brunojorge5887@gmail.com>
2026-04-17 01:11:47 -0300

    corrigindo fetch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   // BUSCAR DO SERVIDOR
   useEffect(() => {
-    fetch("fetch("fetch("http://localhost:3000/produtos")")")
+    fetch("https://loving-wisdom-production-157b.up.railway.app/produtos")
       .then(res => res.json())
       .then(data => setProdutos(data));
   }, []);
@@ -19,47 +19,44 @@ function App() {
     <div style={{ padding: "20px" }}>
       <h1>Minha Loja</h1>
 
-
       <h2>Produtos</h2>
       {produtos.map(p => (
-        <div key={p.id}>
+        <div key={p._id}>
           <h3>{p.nome}</h3>
           <p>R$ {p.preco}</p>
           <button onClick={() => adicionarAoCarrinho(p)}>
-
             Comprar
           </button>
         </div>
       ))}
 
-
       <h2>Carrinho</h2>
 
       <h2>Adicionar Produto</h2>
 
-<input id="nome" placeholder="Nome do produto" />
-<input id="preco" placeholder="Preço" />
+      <input id="nome" placeholder="Nome do produto" />
+      <input id="preco" placeholder="Preço" />
 
-<button onClick={async () => {
-  const nome = document.getElementById("nome").value;
-  const preco = document.getElementById("preco").value;
+      <button onClick={async () => {
+        const nome = document.getElementById("nome").value;
+        const preco = document.getElementById("preco").value;
 
-  await fetch("fetch("https://loving-wisdom-production-157b.up.railway.app/produtos")", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      nome,
-      preco
-    })
-  });
+        await fetch("https://loving-wisdom-production-157b.up.railway.app/produtos", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            nome,
+            preco
+          })
+        });
 
-  alert("Produto adicionado!");
-  window.location.reload();
-}}>
-  Adicionar Produto
-</button>
+        alert("Produto adicionado!");
+        window.location.reload();
+      }}>
+        Adicionar Produto
+      </button>
 
       {carrinho.map((item, i) => (
         <p key={i}>{item.nome}</p>
