c6aca881bd7505dd91be40734b4f62d17e0b112e
Apple <brunojorge5887@gmail.com>
2026-04-17 01:18:14 -0300

    corrigindo backend

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,5 @@ app.post('/produtos', async (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Servidor rodando");
-});
-  console.log("Servidor rodando em http://localhost:3000");
+  console.log("Servidor rodando na porta " + PORT);
 });
