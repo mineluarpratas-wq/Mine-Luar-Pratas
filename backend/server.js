5b95d4123ab69530b3359f938aed1973089391c4
Apple <brunojorge5887@gmail.com>
2026-04-16 23:45:46 -0300

    primeiro commit

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// CONECTAR AO BANCO
+mongoose.connect("mongodb://127.0.0.1:27017/loja");
+
+// MODELO DO PRODUTO
+const Produto = mongoose.model("Produto", {
+  nome: String,
+  preco: Number
+});
+
+// LISTAR PRODUTOS
+app.get('/produtos', async (req, res) => {
+  const produtos = await Produto.find();
+  res.json(produtos);
+});
+
+// ADICIONAR PRODUTO
+app.post('/produtos', async (req, res) => {
+  const novo = new Produto(req.body);
+  await novo.save();
+  res.json({ ok: true });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000");
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [produtos, setProdutos] = useState([]);
+  const [carrinho, setCarrinho] = useState([]);
+
+  // BUSCAR DO SERVIDOR
+  useEffect(() => {
+    fetch("http://localhost:3000/produtos")
+      .then(res => res.json())
+      .then(data => setProdutos(data));
+  }, []);
+
+  function adicionarAoCarrinho(produto) {
+    setCarrinho([...carrinho, produto]);
+  }
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Minha Loja</h1>
+
+
+      <h2>Produtos</h2>
+      {produtos.map(p => (
+        <div key={p.id}>
+          <h3>{p.nome}</h3>
+          <p>R$ {p.preco}</p>
+          <button onClick={() => adicionarAoCarrinho(p)}>
+
+            Comprar
+          </button>
+        </div>
+      ))}
+
+
+      <h2>Carrinho</h2>
+
+    
+      <h2>Adicionar Produto</h2>
+
+<input id="nome" placeholder="Nome do produto" />
+<input id="preco" placeholder="Preço" />
+
+<button onClick={async () => {
+  const nome = document.getElementById("nome").value;
+  const preco = document.getElementById("preco").value;
+
+  await fetch("http://localhost:3000/produtos", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      nome,
+      preco
+    })
+  });
+
+  alert("Produto adicionado!");
+  window.location.reload();
+}}>
+  Adicionar Produto
+</button>
+
+      {carrinho.map((item, i) => (
+        <p key={i}>{item.nome}</p>
+      ))}
+    </div>
+  );
+}
+
+export default App;
